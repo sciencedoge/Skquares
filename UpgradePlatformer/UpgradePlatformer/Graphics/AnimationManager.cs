1979f182aaac91a9ccba4bfeb587a6f85c664628
bob16795 <prestosilver.ptp@gmail.com>
2021-05-21 08:40:20 -0400

    Started AnimationManager Class

---
+++ UpgradePlatformer/UpgradePlatformer/Graphics/AnimationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpgradePlatformer
+{
+    class AnimationManager
+    {
+        public List<Sprite> Sheet;
+
+        public AnimationManager()
+        {
+            //TODO: Load a binary containing sprite locations into the Sheet var
+        }
+    }
+}
