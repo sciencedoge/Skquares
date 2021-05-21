9a48da6089c0ad18ecff4dd77ed2ffe342d6b1de
bob16795 <prestosilver.ptp@gmail.com>
2021-05-21 18:27:46 -0400

    Initial UIManager Implementation

---
+++ UpgradePlatformer/UpgradePlatformer/UI/UIButton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpgradePlatformer
+{
+    class UIButton : UIElement
+    {
+        public delegate void OnClick();
+        OnClick onClick = new OnClick(() => { });
+        public bool Disabled;
+        public void Update(GameTime gameTime) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Calls a function if Disabled == false
+        /// </summary>
+        /// <param name="at">where the button UIElement was clicked 0,0 being the top corner</param>
+        public void WhenClicked(Point at)
+        {
+            if (Disabled) return;
+
+            onClick();
+        }
+    }
+}
