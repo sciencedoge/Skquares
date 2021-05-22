9629e16c5913f62f521d25efbf218bc088e8f961
bob16795 <prestosilver.ptp@gmail.com>
2021-05-22 09:36:28 -0400

    added some random python code

--- SpriteSplitter/main.py
+++ SpriteSplitter/main.py
@@ -1,1 +1,23 @@
-import pygame
+from tkinter import *
+
+
+ws = Tk()
+ws.title('PythonGuides')
+ws.geometry('300x300')
+ws.config(bg='#345')
+
+canvas = Canvas(
+    ws,
+    height=200,
+    width=200,
+    bg="#fff"
+    )
+    
+canvas.pack()
+
+canvas.create_rectangle(
+    30, 30, 180, 120,
+    outline="#fb0",
+    fill="#fb0")
+
+ws.mainloop()
