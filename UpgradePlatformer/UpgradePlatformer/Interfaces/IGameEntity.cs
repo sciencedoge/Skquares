96a3fe2448702b290f0359731ace935a1f7194f0
sciencedoge <xxepicboixxgt@gmail.com>
2021-05-22 13:13:01 -0400

    Created the main interfaces for the game

---
+++ UpgradePlatformer/UpgradePlatformer/Interfaces/IGameEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace UpgradePlatformer.Interfaces
+{
+    //HEADER========================================================
+    //Name: Sami Chamberlain, Preston Precourt
+    //Date: 5/22/2021
+    //Purpose: Provides the groundwork for all game related entities
+    //==============================================================
+    interface IGameEntity
+    {
+        //Properties
+
+        /// <summary>
+        /// returns whether or not an entity
+        /// is active
+        /// </summary>
+        public bool IsActive { get; }
+
+        /// <summary>
+        /// gets or sets the current HP of the
+        /// entity
+        /// </summary>
+        public int CurrentHP { get; set; }
+
+        /// <summary>
+        /// gets or sets the max HP of the
+        /// entity
+        /// </summary>
+        public int MaxHP { get; set; }
+
+        //Methods
+
+        /// <summary>
+        /// Draws an entity to the screen
+        /// </summary>
+        /// <param name="sb">_spriteBatch</param>
+        /// <param name="gt">gameTime</param>
+        public void Draw(SpriteBatch sb, GameTime gt);
+
+        /// <summary>
+        /// Updates elements of 
+        /// </summary>
+        /// <param name="gt"></param>
+        public void Update(GameTime gt);
+
+    }
+}
---
+++ UpgradePlatformer/UpgradePlatformer/Interfaces/IHostile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpgradePlatformer.Interfaces
+{
+    //HEADER========================================
+    //Name: Sami Chamberlain, Preston Precourt
+    //Date: 5/22/2021
+    //Purpose: Provides functionality for hostility
+    //==============================================
+    interface IHostile : IGameEntity
+    {
+        //Properties
+
+        /// <summary>
+        /// Gets the amount of damage an
+        /// entity can deal
+        /// </summary>
+        public int Damage { get; }
+
+    }
+}
