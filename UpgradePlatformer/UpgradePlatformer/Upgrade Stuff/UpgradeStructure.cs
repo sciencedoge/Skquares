9e271dcfdf4c0a017c9aaf67937926c8143f7759
sciencedoge <xxepicboixxgt@gmail.com>
2021-05-26 17:05:33 -0400

    Finished unfinished section of UpgradeStructure

--- UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeStructure.cs
+++ UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeStructure.cs
@@ -14,7 +14,7 @@ namespace UpgradePlatformer.Upgrade_Stuff
         public UpgradeStructure(UpgradeManager manager)
         {
             manager.Add(1, UPGRADE_TYPE.XtraJump, 10);
-            manager.Add(1, UPGRADE_TYPE.);
+            manager.Add(1, UPGRADE_TYPE.Health, 10);
         }
     }
 }
