da0492b38990ab9621dfaadeb68f2d8c5bf07c26
bob16795 <prestosilver.ptp@gmail.com>
2021-05-26 15:59:55 -0400

    added UIGroup class

---
+++ UpgradePlatformer/UpgradePlatformer/UI/UIGroup.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpgradePlatformer.UI
+{
+    class UIGroup : UIElement
+    {
+        public List<UIElement> UIElements;
+        public UIGroup(List<UIElement> elements) {
+            UIElements = elements;
+        }
+        public override UISprite CurrentSprite() { return null; }
+
+        public override void Update(GameTime gameTime) { }
+
+        public override void WhenClicked(Point position)
+        {
+            foreach (UIElement e in UIElements)
+            {
+                if (e.Bounds.Contains(position) && e.IsActive)
+                {
+                    e.WhenClicked(position - e.Bounds.Location);
+                }
+            }
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (UIElement e in UIElements)
+            {
+                e.Draw(gameTime, spriteBatch);
+            }
+        }
+    }
+
+}
