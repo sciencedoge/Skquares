2cce6b5c041183f2e9b15d71ef21719cf6cb9cdd
bob16795 <prestosilver.ptp@gmail.com>
2021-05-29 11:58:27 -0400

    cleaned up FSM namespace

--- UpgradePlatformer/UpgradePlatformer/FSM/FiniteStateMachine.cs
+++ UpgradePlatformer/UpgradePlatformer/FSM/FiniteStateMachine.cs
@@ -4,25 +4,38 @@ using System.Text;
 
 namespace UpgradePlatformer.FSM
 {
-    unsafe class FiniteStateMachine
+    class FiniteStateMachine
     {
         public List<StateMachineState> States;
         public StateMachineState State => States[currentState];
         public int currentState;
 
+        /// <summary>
+        /// creates a finite state machine
+        /// </summary>
+        /// <param name="states">the states the machine can be in</param>
         public FiniteStateMachine(List<StateMachineState> states) {
             currentState = 0;
             States = states;
         }
 
+        /// <summary>
+        /// copys a FSM
+        /// </summary>
+        /// <returns>the clone</returns>
         public FiniteStateMachine Copy() {
             return new FiniteStateMachine(States);
         }
+
+        /// <summary>
+        /// changes the state of the fsm based off flags and the current state
+        /// </summary>
+        /// <param name="id">the flag to set off</param>
         public void SetFlag(int id)
         {
             for (int i = 0; i < States.Count; i++)
             {
-                for (int j = 0; j < States[i].Conds.Count; j++) 
+                for (int j = 0; j < States[i].Conds.Count; j++)
                     if ((States[i].Conds[j]).Id == id)
                         (States[i].Conds[j]).Value = true;
             }
--- UpgradePlatformer/UpgradePlatformer/FSM/StateMachineState.cs
+++ UpgradePlatformer/UpgradePlatformer/FSM/StateMachineState.cs
@@ -7,10 +7,19 @@ namespace UpgradePlatformer.FSM
     public unsafe class StateMachineState
     {
         public List<Flag> Conds;
+        
+        /// <summary>
+        /// creates a state machine state
+        /// </summary>
+        /// <param name="flags">the flags that causes state changes</param>
         public StateMachineState(List<Flag> flags) {
             Conds = flags;
         }
 
+        /// <summary>
+        /// checks for a flag to be set off
+        /// </summary>
+        /// <returns>true if a flag is set off</returns>
         public bool CheckConds()
         {
             for (int j = 0; j < Conds.Count; j++)
@@ -20,6 +29,11 @@ namespace UpgradePlatformer.FSM
                 }
             return false;
         }
+
+        /// <summary>
+        /// gets the next state of the machine
+        /// </summary>
+        /// <returns>the next state</returns>
         public int CheckCondsNext()
         {
             for (int j = 0; j < Conds.Count; j++)
@@ -34,12 +48,18 @@ namespace UpgradePlatformer.FSM
 
     public class Flag
     {
+        /// <summary>
+        /// creates a flag to set off a state machine change
+        /// </summary>
+        /// <param name="id">the flags id this can be repeated</param>
+        /// <param name="nextState">the state the machine should change to when this is set off</param>
         public Flag(int id, int nextState)
         {
             Id = id;
             NextState = nextState;
             Value = false;
         }
+
         public int Id;
         public int NextState;
         public bool Value;
