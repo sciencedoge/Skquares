0821f985ef4eb8375cce75cdc37ef84c006c96b1
bob16795 <prestosilver.ptp@gmail.com>
2021-06-06 14:21:07 -0400

    split player death and cooldown into 2 tests

--- UpgradePlatformer/UpgradePlatformerTests/UnitTest1.cs
+++ UpgradePlatformer/UpgradePlatformerTests/UnitTest1.cs
@@ -11,18 +11,29 @@ namespace UpgradePlatformerTests
         [Fact]
         public void TestPlayerDeathDamage()
         {
-            Player p = new Player(2, 0, new Rectangle(0, 0, 22, 22), 1);
+            Player p = new Player(1, 0, new Rectangle(0, 0, 22, 22), 1);
 
             UpgradePlatformer.Upgrade_Stuff.UpgradeStructure.InitStructure();
 
             p.TakeDamage(1);
-            Assert.Equal(1, p.CurrentHP);
-            Assert.True(p.IsActive);
-            p.Update(new GameTime(TimeSpan.FromMilliseconds(3000), TimeSpan.FromMilliseconds(3000)));
-            Assert.Equal(0, p.cooldown);
-            p.TakeDamage(1);
             Assert.Equal(0, p.CurrentHP);
             Assert.False(p.IsActive);
+        }
+
+        [Fact]
+        public void TestPlayerDamageCoolDown()
+        {
+
+            Player p = new Player(2, 0, new Rectangle(0, 0, 22, 22), 1);
+
+            UpgradePlatformer.Upgrade_Stuff.UpgradeStructure.InitStructure();
+
+            Assert.Equal(0, p.cooldown);
+            p.TakeDamage(1);
+            Assert.Equal(1, p.CurrentHP);
+            Assert.True(p.IsActive);
+            p.Update(new GameTime(TimeSpan.FromMilliseconds(2500), TimeSpan.FromMilliseconds(2500)));
+            Assert.Equal(0, p.cooldown);
         }
     }
     public class UnitTestsPhysics
