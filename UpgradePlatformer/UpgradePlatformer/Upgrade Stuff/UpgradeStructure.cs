4e86039cf33918defd31f1b0d52a2c84e65795e8
bob16795 <prestosilver.ptp@gmail.com>
2021-06-07 16:14:12 -0400

    fix

--- UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeStructure.cs
+++ UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeStructure.cs
@@ -18,17 +18,9 @@ namespace UpgradePlatformer.Upgrade_Stuff
             UpgradeManager.Instance.Upgrades = new List<Upgrade>();
             UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 10);
             UpgradeManager.Instance.Add(1, UpgradeType.EXTRA_JUMP, 10);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 10);
             UpgradeManager.Instance.Add(1, UpgradeType.WEAPON, 0);
             UpgradeManager.Instance.Add(1, UpgradeType.EXTRA_JUMP, 0);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 30);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 30);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 40);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 40);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 50);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 60);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 70);
-            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 80);
+            UpgradeManager.Instance.Add(1, UpgradeType.HEALTH, 10);
         }
         
         public static void ShowMessage(Upgrade ug)
