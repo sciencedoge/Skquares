c82b4cb455b5cde2995779f385cc2d44e7dd5fb8
bob16795 <prestosilver.ptp@gmail.com>
2021-06-07 15:59:07 -0400

    implemented upgrade manager

--- UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeManager.cs
+++ UpgradePlatformer/UpgradePlatformer/Upgrade Stuff/UpgradeManager.cs
@@ -19,6 +19,20 @@ namespace UpgradePlatformer.Upgrade_Stuff
 
         public List<Upgrade> Upgrades;
 
-        private UpgradeManager() { }
+        private UpgradeManager() {
+            Upgrades = new List<Upgrade>();
+        }
+
+        public void Add(int value, UpgradeType type, int price) {
+            Upgrade ug = new Upgrade(value, type, price);
+            Upgrades.Add(ug);    
+        }
+
+        public int GetAmmnt(UpgradeType filter) {
+            int result = 0;
+            foreach (Upgrade ug in Upgrades)
+                result += ug.IsLearned ? ug.Value : 0;
+            return result;
+        }
     }
 }
