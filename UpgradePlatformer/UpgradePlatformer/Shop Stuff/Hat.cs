f2afab2c330651164dd6dfe3b1615e8bbea7218a
bob16795 <prestosilver.ptp@gmail.com>
2021-06-08 12:54:26 -0400

    setup hats sctript

---
+++ UpgradePlatformer/UpgradePlatformer/Shop Stuff/Hat.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UpgradePlatformer.Graphics;
+using UpgradePlatformer.Upgrade_Stuff;
+
+namespace UpgradePlatformer.Shop_Stuff
+{
+    //HEADER=======================================
+    //Name: Sami Chamberlain, Preston Precourt
+    //Date: 5/26/2021
+    //Purpose: Manages hats
+    //============================================
+    public class Hat
+    {
+        public UpgradeBonus bonus;
+        public Sprite sprite;
+
+        public Hat(int texture, int bonus) {
+
+        }
+    }
+}
