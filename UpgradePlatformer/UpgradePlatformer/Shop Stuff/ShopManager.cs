40b54ff8be7992d69e457ccafc4c22f73e0d0541
bob16795 <prestosilver.ptp@gmail.com>
2021-06-08 17:04:24 -0400

    started work on shop manager

--- UpgradePlatformer/UpgradePlatformer/Shop Stuff/ShopManager.cs
+++ UpgradePlatformer/UpgradePlatformer/Shop Stuff/ShopManager.cs
@@ -10,6 +10,14 @@ namespace UpgradePlatformer.Shop_Stuff
     //Date: 5/26/2021
     //Purpose: Manages upgrades
     //============================================
+    
+    public class HatNode
+    {
+        public HatNode parent;
+
+        public Hat hat;
+    }
+
     public class ShopManager
     {
         private static readonly Lazy<ShopManager>
@@ -17,151 +25,26 @@ namespace UpgradePlatformer.Shop_Stuff
             new Lazy<ShopManager>
                 (() => new ShopManager());
         public static ShopManager Instance { get { return lazy.Value; } }
-        private Upgrade root;
 
-        private List<Upgrade> possibleUpgrades;
-
-        /// <summary>
-        /// returns the root upgrade
-        /// </summary>
-        public Upgrade Root
-        {
-            get { return root; }
-            set { root = value; }
-        }
-
+        public List<HatNode> Hats;
 
         /// <summary>
         /// Creates a new UpgradeManager class
         /// </summary>
         public ShopManager() 
         {
-            root = null;
-            possibleUpgrades = new List<Upgrade>();
+            Hats = new List<HatNode>();
         }
 
         /// <summary>
-        /// Adds a node to the tree
+        /// 
         /// </summary>
-        /// <param name="value">value of the upgrade</param>
-        /// <param name="type">type of the upgrade</param>
-        public void Add(int value, UpgradeType type, int cost)
+        /// <param name="hat"></param>
+        /// <param name="parent"></param>
+        public void Add(Hat hat, int parent)
         {
-            if(root == null)
-            {
-                this.root = new Upgrade(value, type, cost);
-            }
-            else
-            {
-                Add(root, value, type, cost);
-            }
-
-        }
-
-        /// <summary>
-        /// Provides functionality for adding upgrades to the tree
-        /// </summary>
-        /// <param name="node">parent node</param>
-        /// <param name="value">value of the new node</param>
-        /// <param name="type">new node type</param>
-        private void Add(Upgrade upgrade, int value, UpgradeType type, int cost)
-        {
-            //Health node, all stored to the LEFT of the tree
-            if(type == UpgradeType.HEALTH)
-            {
-                if (upgrade.Left == null)
-                {
-                    upgrade.Left = new Upgrade(value, type, cost);
-                }
-                else
-                {
-                    Add(upgrade.Left, value, type, cost);
-                }
-            }
-
-            //Jump node, stored to the RIGHT of the tree
-            else if(type == UpgradeType.EXTRA_JUMP)
-            {
-                if(upgrade.Right == null)
-                {
-                    upgrade.Right = new Upgrade(value, type, cost);
-                }
-                else
-                {
-                    Add(upgrade.Right, value, type, cost);
-                }
-            }
-
-            //Speed node, stored to the RIGHT of the tree
-            else if (type == UpgradeType.WEAPON)
-            {
-                if (upgrade.Right == null)
-                {
-                    upgrade.Right = new Upgrade(value, type, cost);
-                }
-                else
-                {
-                    Add(upgrade.Right, value, type, cost);
-                }
-            }
-        }
-
-        public int GetAmmnt(UpgradeType Type, Upgrade upgrade = null) {
-            if (upgrade == null)
-                upgrade = Root;
-            //LRC Data pattern
-            int result = 0;
-
-            if (upgrade == null) return 0;
-
-            if (upgrade.IsLearned)
-            {
-                if (upgrade.Type == Type)
-                    result += 1;
-
-                if (upgrade.Left != null)
-                {
-                    result += GetAmmnt(Type, upgrade.Left);
-                }
-
-                if (upgrade.Right != null)
-                {
-                    result += GetAmmnt(Type, upgrade.Right);
-                }
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Lists all of the upgrades
-        /// purchased by the player
-        /// </summary>
-        public List<Upgrade> CanBeLearned(UpgradeType type = UpgradeType.ANY, Upgrade upgrade = null)
-        { 
-            if (upgrade == null) 
-                upgrade = Root;
-            //LRC Data pattern
-            List<Upgrade> result = new List<Upgrade>();
-
-            if (upgrade.IsLearned)
-            {
-                if(upgrade.Left != null)
-                {
-                    result.AddRange(CanBeLearned(type, upgrade.Left));
-                }
-                
-                if(upgrade.Right != null)
-                {
-                    result.AddRange(CanBeLearned(type, upgrade.Right));
-                }
-            } else {
-                if (type == UpgradeType.ANY || type == upgrade.Type)
-                    return new List<Upgrade> { upgrade };
-                return new List<Upgrade>();
-            }
-
-            return result;
+            var node = new HatNode{hat = hat, parent = Hats[parent]};
+            Hats.Add(node);
         }
     }
 }
