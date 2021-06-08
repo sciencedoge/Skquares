6b8f5ea6bd7b94a58c851cb2a2993020a0ce9088
bob16795 <prestosilver.ptp@gmail.com>
2021-06-08 17:04:16 -0400

    finished hat class

--- UpgradePlatformer/UpgradePlatformer/Shop Stuff/Hat.cs
+++ UpgradePlatformer/UpgradePlatformer/Shop Stuff/Hat.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using UpgradePlatformer.Entities;
 using UpgradePlatformer.Graphics;
 using UpgradePlatformer.Upgrade_Stuff;
 
@@ -16,8 +19,20 @@ namespace UpgradePlatformer.Shop_Stuff
         public UpgradeBonus bonus;
         public Sprite sprite;
 
-        public Hat(int texture, int bonus) {
+        public Hat(int texture, UpgradeType boost, float value) {
+            bonus = new UpgradeBonus{value = value, type = boost};
+            sprite = new Sprite(new Rectangle(35 + 7 * texture, 0, 7, 7), new Vector2(3, 7), Color.White);
+        }
 
+        public void Draw(SpriteBatch sb, GameTime gt) {
+            Player p = EntityManager.Instance.Player();
+            if (p.animation.finiteStateMachine.currentState % 2 == 0) {
+                float wave = MathF.Cos((float)gt.TotalGameTime.TotalSeconds) / 10 + (MathF.PI / 4 * 7);
+                sprite.Draw(sb, p.hitbox.Location + new Point(3, 3) , wave, new Vector2(15, 15));
+            } else {
+                float wave = MathF.Cos((float)gt.TotalGameTime.TotalSeconds) / 10 - (MathF.PI / 4 * 7);
+                sprite.Draw(sb, p.hitbox.Location + new Point(p.hitbox.Size.X - 3, 3) , wave, new Vector2(15, 15));
+            }
         }
     }
 }
