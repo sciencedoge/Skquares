55ed4a52745d91c5b06d72f69073f607e91731ca
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 14:56:20 -0400

    animation cleanup

--- UpgradePlatformer/UpgradePlatformer/Graphics/Animation.cs
+++ UpgradePlatformer/UpgradePlatformer/Graphics/Animation.cs
@@ -1,124 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using UpgradePlatformer.FSM;
 
 namespace UpgradePlatformer.Graphics
 {
-    public class AnimationFSM
+  public class Animation
+  {
+    static Random rand;
+    public List<Sprite> sprites;
+    private int sprite;
+    private float counter;
+    private readonly int framesPerSprite;
+
+    /// <summary>
+    /// creates an animation
+    /// </summary>
+    /// <param name="Sprites">the sprites in the animation</param>
+    /// <param name="FramesPerSprite">the ammount of frames to spend on the sprite</param>
+    public Animation(List<Sprite> Sprites, int FramesPerSprite)
     {
-        public FiniteStateMachine finiteStateMachine;
-        public List<Animation> animations;
-
-        /// <summary>
-        /// creates a fsm based animation
-        /// </summary>
-        /// <param name="fsm">the fsm for the animation</param>
-        /// <param name="Animations">the animations per state machine state</param>
-        public AnimationFSM(FiniteStateMachine fsm, List<Animation> Animations)
-        {
-            finiteStateMachine = fsm;
-            animations = Animations;
-        }
-        
-        /// <summary>
-        /// copys a fsm animation
-        /// </summary>
-        /// <param name="Copy">the fsm animation to copy</param>
-        public AnimationFSM(AnimationFSM Copy)
-        {
-            finiteStateMachine = Copy.finiteStateMachine.Copy();
-            animations = new List<Animation>();
-            foreach (Animation a in Copy.animations)
-                animations.Add(new Animation(a));
-        }
-        
-        /// <summary>
-        /// updates the animations in the animation list
-        /// </summary>
-        /// <param name="gameTime">a GameTime object</param>
-        public void Update(GameTime gameTime)
-        {
-            foreach (Animation a in animations)
-            {
-                a.Update(gameTime);
-            }
-        }
-
-        /// <summary>
-        /// sets off a flag on the fsm
-        /// </summary>
-        /// <param name="id">the id of the flag to set off</param>
-        public void SetFlag(int id)
-        {
-            finiteStateMachine.SetFlag(id);
-        }
-
-        /// <summary>
-        /// draws the current frame of the current animation
-        /// </summary>
-        /// <param name="spriteBatch">a SpriteBatch object</param>
-        /// <param name="position">the position of the sprite</param>
-        /// <param name="rotation">the rotation of the sprite</param>
-        /// <param name="size">the size of the sprite</param>
-        public void Draw(SpriteBatch spriteBatch, Point position, float rotation, Vector2 size)
-        {
-            animations[finiteStateMachine.currentState].Draw(spriteBatch, position, rotation, size);
-        }
+      sprites = Sprites;
+      framesPerSprite = FramesPerSprite;
     }
 
-    public class Animation
+    /// <summary>
+    /// creates an animation
+    /// </summary>
+    /// <param name="Sprites">the sprites in the animation</param>
+    /// <param name="FramesPerSprite">the ammount of frames to spend on the sprite</param>
+    public Animation(Animation Copy)
     {
-        static Random rand;
-        public List<Sprite> sprites;
-        private int sprite;
-        private float counter;
-        private readonly int framesPerSprite;
-        
-        /// <summary>
-        /// creates an animation
-        /// </summary>
-        /// <param name="Sprites">the sprites in the animation</param>
-        /// <param name="FramesPerSprite">the ammount of frames to spend on the sprite</param>
-        public Animation(List<Sprite> Sprites, int FramesPerSprite) {
-            sprites = Sprites;
-            framesPerSprite = FramesPerSprite;
-        }
-
-        /// <summary>
-        /// creates an animation
-        /// </summary>
-        /// <param name="Sprites">the sprites in the animation</param>
-        /// <param name="FramesPerSprite">the ammount of frames to spend on the sprite</param>
-        public Animation(Animation Copy)
-        {
-            if (rand == null) rand = new Random();
-            sprites = Copy.sprites;
-            framesPerSprite = Copy.framesPerSprite;
-            counter += rand.Next(1,1000);
-        }
-
-        /// <summary>
-        /// updates the animation frame
-        /// </summary>
-        /// <param name="gameTime">a GameTime object</param>
-        public void Update(GameTime gameTime) {
-            counter += (float)(gameTime.ElapsedGameTime.TotalSeconds * 60);
-            while (counter > framesPerSprite) { sprite++;  counter -= framesPerSprite; }
-            sprite %= (sprites.Count);
-        }
-
-        /// <summary>
-        /// draws the current frame of the animation
-        /// </summary>
-        /// <param name="spriteBatch">a SpriteBatch object</param>
-        /// <param name="position">the position of the sprite</param>
-        /// <param name="rotation">the rotation of the sprite</param>
-        /// <param name="size">the size of the sprite</param>
-        public void Draw(SpriteBatch spriteBatch, Point position, float rotation, Vector2 size) {
-            sprites[sprite].Draw(spriteBatch, position, rotation, size);
-        }
+      if (rand == null) rand = new Random();
+      sprites = Copy.sprites;
+      framesPerSprite = Copy.framesPerSprite;
+      counter += rand.Next(1, 1000);
     }
+
+    /// <summary>
+    /// updates the animation frame
+    /// </summary>
+    /// <param name="gameTime">a GameTime object</param>
+    public void Update(GameTime gameTime)
+    {
+      counter += (float)(gameTime.ElapsedGameTime.TotalSeconds * 60);
+      while (counter > framesPerSprite) { sprite++; counter -= framesPerSprite; }
+      sprite %= (sprites.Count);
+    }
+
+    /// <summary>
+    /// draws the current frame of the animation
+    /// </summary>
+    /// <param name="spriteBatch">a SpriteBatch object</param>
+    /// <param name="position">the position of the sprite</param>
+    /// <param name="rotation">the rotation of the sprite</param>
+    /// <param name="size">the size of the sprite</param>
+    public void Draw(SpriteBatch spriteBatch, Point position, float rotation, Vector2 size)
+    {
+      sprites[sprite].Draw(spriteBatch, position, rotation, size);
+    }
+  }
 }
--- UpgradePlatformer/UpgradePlatformer/Graphics/Sprites.cs
+++ UpgradePlatformer/UpgradePlatformer/Graphics/Sprites.cs
@@ -2,131 +2,131 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace UpgradePlatformer.Graphics
 {
-    public class Sprite
+  public class Sprite
+  {
+    public static List<Effect> Shaders;
+    public static bool Dim, Light;
+    public static Texture2D texture;
+    public static GraphicsDeviceManager graphics;
+    public SpriteEffects effects = SpriteEffects.None;
+    public Rectangle Position;
+    public Vector2 Origin;
+    public Color TintColor;
+
+    /// <summary>
+    /// creates a sprite object
+    /// </summary>
+    /// <param name="position">the position of the texture on the sheet</param>
+    /// <param name="origin">the center of the Sprite</param>
+    /// <param name="tintColor">the color to tint the sprite</param>
+    public Sprite(Rectangle position, Vector2 origin, Color tintColor)
     {
-        public static List<Effect> Shaders;
-        public static bool Dim, Light;
-        public static Texture2D texture;
-        public static GraphicsDeviceManager graphics;
-        public SpriteEffects effects = SpriteEffects.None;
-        public Rectangle Position;
-        public Vector2 Origin;
-        public Color TintColor;
-        
-        /// <summary>
-        /// creates a sprite object
-        /// </summary>
-        /// <param name="position">the position of the texture on the sheet</param>
-        /// <param name="origin">the center of the Sprite</param>
-        /// <param name="tintColor">the color to tint the sprite</param>
-        public Sprite(Rectangle position, Vector2 origin, Color tintColor)
-        {
-            Position = position;
-            Origin = origin;
-            TintColor = tintColor;
-        }
-        
-        /// <summary>
-        /// renders a sprite object
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        /// <param name="renderPosition">where to render the sprite</param>
-        /// <param name="rotation">the rotation of the sprite</param>
-        public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation)
-        {
-            Draw(spriteBatch, renderPosition, rotation, Position.Size.ToVector2());
-        }
-
-        /// <summary>
-        /// renders a sprite object
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        /// <param name="renderPosition">where to render the sprite</param>
-        /// <param name="rotation">the rotation of the sprite</param>
-        public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation, SpriteEffects effect)
-        {
-            this.effects = effect;
-            Draw(spriteBatch, renderPosition, rotation, Position.Size.ToVector2());
-        }
-
-        /// <summary>
-        /// renders a sprite object
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        /// <param name="renderPosition">where to render the sprite</param>
-        /// <param name="rotation">the rotation of the sprite</param>
-        /// <param name="Size">the size to render the sprite</param>
-        public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation, Vector2 Size)
-        {
-            renderPosition.X = (int)(GetScale() * renderPosition.X);
-            renderPosition.Y = (int)(GetScale() * renderPosition.Y);
-            Size.X = (int)(GetScale() * (Size.X)) + 1;
-            Size.Y = (int)(GetScale() * (Size.Y)) + 1;
-            Color c = TintColor;
-            if (Dim && Light)
-            {
-                c.R /= 2;
-                c.G /= 2;
-                c.B /= 2;
-            }
-            Rectangle renderRect = Position;
-            renderRect.Location = renderPosition + new Point(0,(int)(40f * GetScale())) + GetOrigin();
-            renderRect.Size = Size.ToPoint();
-            spriteBatch.Draw(texture, renderRect, Position, c, rotation, Origin, effects, 0f);
-        }
-
-        /// <summary>
-        /// gets the origin of the window render area
-        /// </summary>
-        /// <returns></returns>
-        public static Point GetOrigin()
-        {
-            Point Size = new Point(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight - (int)(40f * GetScale()));
-            if (Size.X > Size.Y)
-            {
-                return new Point((Size.X - Size.Y) / 2, 0);
-            }
-            else
-            {
-                return new Point(0, (Size.Y - Size.X) / 2);
-            }
-        }
-
-        /// <summary>
-        /// returns a rect of the render positions
-        /// </summary>
-        /// <returns></returns>
-        public static Rectangle GetRect()
-        {
-            Rectangle result = new Rectangle();
-            result.Location = GetOrigin();
-            result.Size = new Point((int)MathF.Min(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight - 40f * GetScale()));
-            return result;
-        }
-
-        /// <summary>
-        /// gets the scale of the window
-        /// </summary>
-        /// <returns>the size of one unit in the window</returns>
-        public static float GetScale()
-        {
-            return MathF.Min(graphics.PreferredBackBufferWidth / 630f, graphics.PreferredBackBufferHeight / 670f) ;
-        }
-
-        /// <summary>
-        /// copys a sprite, problaby not needed
-        /// </summary>
-        /// <returns>a new sprite</returns>
-        public Sprite Copy() {
-            Sprite spr = new Sprite(Position, Origin, TintColor)
-            {
-                effects = effects
-            };
-            return spr;
-        }
+      Position = position;
+      Origin = origin;
+      TintColor = tintColor;
     }
+
+    /// <summary>
+    /// renders a sprite object
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    /// <param name="renderPosition">where to render the sprite</param>
+    /// <param name="rotation">the rotation of the sprite</param>
+    public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation)
+    {
+      Draw(spriteBatch, renderPosition, rotation, Position.Size.ToVector2());
+    }
+
+    /// <summary>
+    /// renders a sprite object
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    /// <param name="renderPosition">where to render the sprite</param>
+    /// <param name="rotation">the rotation of the sprite</param>
+    public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation, SpriteEffects effect)
+    {
+      this.effects = effect;
+      Draw(spriteBatch, renderPosition, rotation, Position.Size.ToVector2());
+    }
+
+    /// <summary>
+    /// renders a sprite object
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    /// <param name="renderPosition">where to render the sprite</param>
+    /// <param name="rotation">the rotation of the sprite</param>
+    /// <param name="Size">the size to render the sprite</param>
+    public void Draw(SpriteBatch spriteBatch, Point renderPosition, float rotation, Vector2 Size)
+    {
+      renderPosition.X = (int)(GetScale() * renderPosition.X);
+      renderPosition.Y = (int)(GetScale() * renderPosition.Y);
+      Size.X = (int)(GetScale() * (Size.X)) + 1;
+      Size.Y = (int)(GetScale() * (Size.Y)) + 1;
+      Color c = TintColor;
+      if (Dim && Light)
+      {
+        c.R /= 2;
+        c.G /= 2;
+        c.B /= 2;
+      }
+      Rectangle renderRect = Position;
+      renderRect.Location = renderPosition + new Point(0, (int)(40f * GetScale())) + GetOrigin();
+      renderRect.Size = Size.ToPoint();
+      spriteBatch.Draw(texture, renderRect, Position, c, rotation, Origin, effects, 0f);
+    }
+
+    /// <summary>
+    /// gets the origin of the window render area
+    /// </summary>
+    /// <returns></returns>
+    public static Point GetOrigin()
+    {
+      Point Size = new Point(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight - (int)(40f * GetScale()));
+      if (Size.X > Size.Y)
+      {
+        return new Point((Size.X - Size.Y) / 2, 0);
+      }
+      else
+      {
+        return new Point(0, (Size.Y - Size.X) / 2);
+      }
+    }
+
+    /// <summary>
+    /// returns a rect of the render positions
+    /// </summary>
+    /// <returns></returns>
+    public static Rectangle GetRect()
+    {
+      Rectangle result = new Rectangle();
+      result.Location = GetOrigin();
+      result.Size = new Point((int)MathF.Min(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight - 40f * GetScale()));
+      return result;
+    }
+
+    /// <summary>
+    /// gets the scale of the window
+    /// </summary>
+    /// <returns>the size of one unit in the window</returns>
+    public static float GetScale()
+    {
+      return MathF.Min(graphics.PreferredBackBufferWidth / 630f, graphics.PreferredBackBufferHeight / 670f);
+    }
+
+    /// <summary>
+    /// copys a sprite, problaby not needed
+    /// </summary>
+    /// <returns>a new sprite</returns>
+    public Sprite Copy()
+    {
+      Sprite spr = new Sprite(Position, Origin, TintColor)
+      {
+        effects = effects
+      };
+      return spr;
+    }
+  }
 }
