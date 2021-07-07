7801062d2be14143dca742e32c131f1824210ea0
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 15:04:25 -0400

    cleanup sounds

--- UpgradePlatformer/UpgradePlatformer/Music/SoundManager.cs
+++ UpgradePlatformer/UpgradePlatformer/Music/SoundManager.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using UpgradePlatformer.Levels;
 
