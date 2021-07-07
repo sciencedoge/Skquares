71a5f17b5f233d97d7e6ec9bd36202d583cf875b
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 15:00:07 -0400

    cleanup levels

--- UpgradePlatformer/UpgradePlatformer/Levels/LevelManager.cs
+++ UpgradePlatformer/UpgradePlatformer/Levels/LevelManager.cs
@@ -2,173 +2,174 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UpgradePlatformer.Entities;
 using UpgradePlatformer.Graphics;
-using UpgradePlatformer.Input;
-using UpgradePlatformer.Upgrade_Stuff;
 using UpgradePlatformer.Music;
 
 namespace UpgradePlatformer.Levels
 {
-    public class LevelManager
+  public class LevelManager
+  {
+    /// <summary>
+    /// Singleton Stuff
+    /// </summary>
+    private static readonly Lazy<LevelManager>
+        lazy =
+        new Lazy<LevelManager>
+            (() => new LevelManager());
+    public static LevelManager Instance { get { return lazy.Value; } }
+
+    public static List<Texture2D> BackDrops;
+    public List<World> Worlds;
+    int activeWorld;
+    int _activeWorld;
+
+    /// <summary>
+    /// Updates the level manager
+    /// </summary>
+    public void Update()
     {
-        /// <summary>
-        /// Singleton Stuff
-        /// </summary>
-        private static readonly Lazy<LevelManager>
-            lazy =
-            new Lazy<LevelManager>
-                (() => new LevelManager());
-        public static LevelManager Instance { get { return lazy.Value; } }
-
-        public static List<Texture2D> BackDrops;
-        public List<World> Worlds;
-        int activeWorld;
-        int _activeWorld;
-        
-        /// <summary>
-        /// Updates the level manager
-        /// </summary>
-        public void Update() {
-            if (activeWorld != _activeWorld) {
-                ActiveWorld().SetLevel(0);
-                activeWorld = _activeWorld;
-                EntityManager.Instance.Clean(true);
-                Worlds[activeWorld].LoadEntities(true);
-                SoundManager.Instance.PlayMusic(0);
-            }
-            ActiveWorld().Update();
-        }
-
-        /// <summary>
-        /// creates a level manager
-        /// </summary>
-        public LevelManager()
-        {
-            Reset();
-        }
-
-        /// <summary>
-        /// draws the active level in the active world
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        public void Draw(SpriteBatch spriteBatch, bool background)
-        {
-            if (background)
-            {
-                Rectangle r = Sprite.GetRect();
-                r.Location += new Vector2(0, 40 * Sprite.GetScale()).ToPoint();
-                spriteBatch.Draw(ActiveWorld().Backdrop, r, Color.White);
-            }
-            Worlds[activeWorld].Draw(spriteBatch, background);
-        }
-
-        /// <summary>
-        /// draws the active level in the active world
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        public void DrawLightMap(SpriteBatch spriteBatch)
-        {
-            Worlds[activeWorld].DrawLightMap(spriteBatch);
-        }
-
-        /// <summary>
-        /// sets the active world
-        /// </summary>
-        /// <param name="id">the world id</param>
-        public void SetWorld(int id) {
-            if (id < 0)
-                _activeWorld = Worlds.Count - 1;
-            else
-            _activeWorld = id % (Worlds.Count);
-        }
-
-        /// <summary>
-        /// goes to the next world
-        /// </summary>
-        public void Next()
-        {
-            SetWorld(activeWorld + 1);
-        }
-
-        /// <summary>
-        /// goes to the previous world
-        /// </summary>
-        public void Prev()
-        {
-            SetWorld(activeWorld - 1);
-        }
-        
-        /// <summary>
-        /// resets the level manager
-        /// </summary>
-        public void Reset()
-        {
-            Worlds = new List<World>();
-            _activeWorld = 0;
-            Worlds.Add(new World(BackDrops[0], 0, 1, 2, "menu",     false, new List<int> { -1, -1 }));
-            Worlds.Add(new World(BackDrops[0], 0, 2, 6, "tutorial", false, new List<int> { -1, -1, -1, -1, -1, 0 }));
-            Worlds.Add(new World(BackDrops[0], 1, 1, 3, "clouds",   false, new List<int> { -1, -1, 1 }));
-            Worlds.Add(new World(BackDrops[0], 1, 2, 4, "clouds 2", false, new List<int> { -1, -1, -1, -1 }));
-            Worlds.Add(new World(BackDrops[0], 1, 3, 1, "t1",       false, new List<int> { -1}));
-            Worlds.Add(new World(BackDrops[0], 1, 4, 1, "t2",       false, new List<int> { -1}));
-            Worlds.Add(new World(BackDrops[0], 1, 5, 1, "t3",       false, new List<int> { -1}));
-            Worlds.Add(new World(BackDrops[1], 2, 2, 2, "preCaves", false, new List<int> { -1, -1 }));
-            Worlds.Add(new World(BackDrops[1], 2, 1, 4, "caves",    true , new List<int> { -1, -1, 2, 3 }));
-        }
-
-        /// <summary>
-        /// adds a tile to the list of collected tiles
-        /// </summary>
-        /// <param name="t"></param>
-        public void Collect(Tile t) {
-            ActiveLevel().Collected.Add(new LevelCollectedEntity(t));
-        }
-
-        public bool UpdateCheck() => _activeWorld != activeWorld || ActiveWorld().CheckUpdate();
-
-        /// <summary>
-        /// sets the active level
-        /// </summary>
-        /// <param name="id">the id of the level</param>
-        public void SetLevel(int id) => ActiveWorld().SetLevel(id);
-        /// <summary>
-        /// gets the spawners in the active level
-        /// </summary>
-        /// <returns>the spawners</returns>
-        public List<Tile> Spawners() => Worlds[activeWorld].GetSpawners();
-        /// <summary>
-        /// gets collisions for the level
-        /// </summary>
-        /// <param name="r">the rectangle of tiles</param>
-        /// <returns></returns>
-        public List<Tile> GetCollisions(Rectangle r) => Worlds[activeWorld].GetCollisions(r);
-        /// <summary>
-        /// gets the active level
-        /// </summary>
-        /// <returns>the level</returns>
-        public Level ActiveLevel() => ActiveWorld().ActiveLevel();
-        /// <summary>
-        /// gets the active world
-        /// </summary>
-        /// <returns>the world</returns>
-        public World ActiveWorld() => Worlds[activeWorld];
-        /// <summary>
-        /// gets the active level name
-        /// </summary>
-        /// <returns>the active levels name</returns>
-        public String ActiveLevelName() => ActiveWorld().ActiveLevelName();
-        /// <summary>
-        /// gets the id of the active level
-        /// </summary>
-        /// <returns>the id</returns>
-        public int ActiveLevelNum() => ActiveWorld().ActiveLevelNum();
-        /// <summary>
-        /// gets the id of the active world
-        /// </summary>
-        /// <returns>the id</returns>
-        public int ActiveWorldNum() => activeWorld;
-        public bool Light => ActiveLevel().Light;
+      if (activeWorld != _activeWorld)
+      {
+        ActiveWorld().SetLevel(0);
+        activeWorld = _activeWorld;
+        EntityManager.Instance.Clean(true);
+        Worlds[activeWorld].LoadEntities(true);
+        SoundManager.Instance.PlayMusic(0);
+      }
+      ActiveWorld().Update();
     }
+
+    /// <summary>
+    /// creates a level manager
+    /// </summary>
+    public LevelManager()
+    {
+      Reset();
+    }
+
+    /// <summary>
+    /// draws the active level in the active world
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    public void Draw(SpriteBatch spriteBatch, bool background)
+    {
+      if (background)
+      {
+        Rectangle r = Sprite.GetRect();
+        r.Location += new Vector2(0, 40 * Sprite.GetScale()).ToPoint();
+        spriteBatch.Draw(ActiveWorld().Backdrop, r, Color.White);
+      }
+      Worlds[activeWorld].Draw(spriteBatch, background);
+    }
+
+    /// <summary>
+    /// draws the active level in the active world
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    public void DrawLightMap(SpriteBatch spriteBatch)
+    {
+      Worlds[activeWorld].DrawLightMap(spriteBatch);
+    }
+
+    /// <summary>
+    /// sets the active world
+    /// </summary>
+    /// <param name="id">the world id</param>
+    public void SetWorld(int id)
+    {
+      if (id < 0)
+        _activeWorld = Worlds.Count - 1;
+      else
+        _activeWorld = id % (Worlds.Count);
+    }
+
+    /// <summary>
+    /// goes to the next world
+    /// </summary>
+    public void Next()
+    {
+      SetWorld(activeWorld + 1);
+    }
+
+    /// <summary>
+    /// goes to the previous world
+    /// </summary>
+    public void Prev()
+    {
+      SetWorld(activeWorld - 1);
+    }
+
+    /// <summary>
+    /// resets the level manager
+    /// </summary>
+    public void Reset()
+    {
+      Worlds = new List<World>();
+      _activeWorld = 0;
+      Worlds.Add(new World(BackDrops[0], 0, 1, 2, "menu", false, new List<int> { -1, -1 }));
+      Worlds.Add(new World(BackDrops[0], 0, 2, 6, "tutorial", false, new List<int> { -1, -1, -1, -1, -1, 0 }));
+      Worlds.Add(new World(BackDrops[0], 1, 1, 3, "clouds", false, new List<int> { -1, -1, 1 }));
+      Worlds.Add(new World(BackDrops[0], 1, 2, 4, "clouds 2", false, new List<int> { -1, -1, -1, -1 }));
+      Worlds.Add(new World(BackDrops[0], 1, 3, 1, "t1", false, new List<int> { -1 }));
+      Worlds.Add(new World(BackDrops[0], 1, 4, 1, "t2", false, new List<int> { -1 }));
+      Worlds.Add(new World(BackDrops[0], 1, 5, 1, "t3", false, new List<int> { -1 }));
+      Worlds.Add(new World(BackDrops[1], 2, 2, 2, "preCaves", false, new List<int> { -1, -1 }));
+      Worlds.Add(new World(BackDrops[1], 2, 1, 4, "caves", true, new List<int> { -1, -1, 2, 3 }));
+    }
+
+    /// <summary>
+    /// adds a tile to the list of collected tiles
+    /// </summary>
+    /// <param name="t"></param>
+    public void Collect(Tile t)
+    {
+      ActiveLevel().Collected.Add(new LevelCollectedEntity(t));
+    }
+
+    public bool UpdateCheck() => _activeWorld != activeWorld || ActiveWorld().CheckUpdate();
+
+    /// <summary>
+    /// sets the active level
+    /// </summary>
+    /// <param name="id">the id of the level</param>
+    public void SetLevel(int id) => ActiveWorld().SetLevel(id);
+    /// <summary>
+    /// gets the spawners in the active level
+    /// </summary>
+    /// <returns>the spawners</returns>
+    public List<Tile> Spawners() => Worlds[activeWorld].GetSpawners();
+    /// <summary>
+    /// gets collisions for the level
+    /// </summary>
+    /// <param name="r">the rectangle of tiles</param>
+    /// <returns></returns>
+    public List<Tile> GetCollisions(Rectangle r) => Worlds[activeWorld].GetCollisions(r);
+    /// <summary>
+    /// gets the active level
+    /// </summary>
+    /// <returns>the level</returns>
+    public Level ActiveLevel() => ActiveWorld().ActiveLevel();
+    /// <summary>
+    /// gets the active world
+    /// </summary>
+    /// <returns>the world</returns>
+    public World ActiveWorld() => Worlds[activeWorld];
+    /// <summary>
+    /// gets the active level name
+    /// </summary>
+    /// <returns>the active levels name</returns>
+    public String ActiveLevelName() => ActiveWorld().ActiveLevelName();
+    /// <summary>
+    /// gets the id of the active level
+    /// </summary>
+    /// <returns>the id</returns>
+    public int ActiveLevelNum() => ActiveWorld().ActiveLevelNum();
+    /// <summary>
+    /// gets the id of the active world
+    /// </summary>
+    /// <returns>the id</returns>
+    public int ActiveWorldNum() => activeWorld;
+    public bool Light => ActiveLevel().Light;
+  }
 }
--- UpgradePlatformer/UpgradePlatformer/Levels/World.cs
+++ UpgradePlatformer/UpgradePlatformer/Levels/World.cs
@@ -2,137 +2,139 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UpgradePlatformer.Entities;
-using UpgradePlatformer.Upgrade_Stuff;
 
 namespace UpgradePlatformer.Levels
 {
-    public class World
+  public class World
+  {
+    public List<Level> Levels;
+    public List<int> upgrades;
+    public String Music;
+    int activeLevel;
+    int _activeLevel;
+    bool Light = false;
+    public Texture2D Backdrop;
+    public bool CheckUpdate() => _activeLevel != activeLevel;
+
+    /// <summary>
+    /// updates the level if needed
+    /// </summary>
+    public void Update()
     {
-        public List<Level> Levels;
-        public List<int> upgrades;
-        public String Music;
-        int activeLevel;
-        int _activeLevel;
-        bool Light = false;
-        public Texture2D Backdrop;
-        public bool CheckUpdate() => _activeLevel != activeLevel;
-        
-        /// <summary>
-        /// updates the level if needed
-        /// </summary>
-        public void Update() {
-            if (activeLevel != _activeLevel) {
-                activeLevel = _activeLevel;
-                EntityManager.Instance.Clean(false);
-                Levels[activeLevel].LoadEntities(false);
-            }
-        }
-
-        /// <summary>
-        /// loads the entities in the active level
-        /// </summary>
-        /// <param name="player">whether to spawn a player</param>
-        public void LoadEntities(bool player) =>
-            Levels[activeLevel].LoadEntities(player);
-
-        /// <summary>
-        /// creates a world object
-        /// </summary>
-        /// <param name="levels">the levels in the world</param>
-        /// <param name="defaultLevel">the default level</param>
-        public World(Texture2D backdrop, int world_num, int sub_num, int levels, String music, bool light, List<int> Upgrades)
-        {
-            Backdrop = backdrop;
-            Music = music;
-            upgrades = Upgrades;
-            Light = light;
-            Levels = new List<Level>();
-            for (int i = 1; i <= levels; i++)
-                Load($"{world_num}_{sub_num}_{i}");
-        }
-        
-        /// <summary>
-        /// loads a level into memory
-        /// </summary>
-        /// <param name="Name"></param>
-        public void Load(String Name)
-        {
-            Levels.Add(new Level(Name, Light, upgrades[Levels.Count]));
-        }
-
-        /// <summary>
-        /// draws the active level
-        /// </summary>
-        /// <param name="spriteBatch">a SpriteBatch object</param>
-        public void Draw(SpriteBatch spriteBatch, bool background)
-        {
-            Levels[activeLevel].Draw(spriteBatch, background);
-        }
-
-        /// <summary>
-        /// draws the active level in the active world
-        /// </summary>
-        /// <param name="spriteBatch">the SpriteBatch object</param>
-        public void DrawLightMap(SpriteBatch spriteBatch)
-        {
-            Levels[activeLevel].DrawLightMap(spriteBatch);
-        }
-
-        /// <summary>
-        /// sets the active level
-        /// </summary>
-        /// <param name="id">the id of the level</param>
-        public void SetLevel(int id) {
-            if (id < 0) {
-                _activeLevel = Levels.Count - 1;
-            }
-            else
-            _activeLevel = id % (Levels.Count);
-        }
-
-        /// <summary>
-        /// goes to the next level
-        /// </summary>
-        public void Next()
-        {
-            SetLevel(activeLevel + 1);
-        }
-
-        /// <summary>
-        /// goes to the previous level
-        /// </summary>
-        public void Prev()
-        {
-            SetLevel(activeLevel - 1);
-        }
-
-        /// <summary>
-        /// gets the spawners of the active level
-        /// </summary>
-        /// <returns>the spawner tiles</returns>
-        public List<Tile> GetSpawners() => Levels[activeLevel].GetSpawners();
-        /// <summary>
-        /// gets collisions for the current level
-        /// </summary>
-        /// <param name="r">a rectangle to check</param>
-        /// <returns>the colliding tiles</returns>
-        public List<Tile> GetCollisions(Rectangle r) => Levels[activeLevel].GetCollisions(r);
-        /// <summary>
-        /// gets the active level
-        /// </summary>
-        /// <returns>the active level</returns>
-        public Level ActiveLevel() => Levels[activeLevel];
-        /// <summary>
-        /// gets the active levels name
-        /// </summary>
-        /// <returns>the name</returns>
-        public String ActiveLevelName() => Levels[activeLevel].Name;
-        /// <summary>
-        /// gets the id of the active level
-        /// </summary>
-        /// <returns>the id</returns>
-        public int ActiveLevelNum() => activeLevel;
+      if (activeLevel != _activeLevel)
+      {
+        activeLevel = _activeLevel;
+        EntityManager.Instance.Clean(false);
+        Levels[activeLevel].LoadEntities(false);
+      }
     }
+
+    /// <summary>
+    /// loads the entities in the active level
+    /// </summary>
+    /// <param name="player">whether to spawn a player</param>
+    public void LoadEntities(bool player) =>
+        Levels[activeLevel].LoadEntities(player);
+
+    /// <summary>
+    /// creates a world object
+    /// </summary>
+    /// <param name="levels">the levels in the world</param>
+    /// <param name="defaultLevel">the default level</param>
+    public World(Texture2D backdrop, int world_num, int sub_num, int levels, String music, bool light, List<int> Upgrades)
+    {
+      Backdrop = backdrop;
+      Music = music;
+      upgrades = Upgrades;
+      Light = light;
+      Levels = new List<Level>();
+      for (int i = 1; i <= levels; i++)
+        Load($"{world_num}_{sub_num}_{i}");
+    }
+
+    /// <summary>
+    /// loads a level into memory
+    /// </summary>
+    /// <param name="Name"></param>
+    public void Load(String Name)
+    {
+      Levels.Add(new Level(Name, Light, upgrades[Levels.Count]));
+    }
+
+    /// <summary>
+    /// draws the active level
+    /// </summary>
+    /// <param name="spriteBatch">a SpriteBatch object</param>
+    public void Draw(SpriteBatch spriteBatch, bool background)
+    {
+      Levels[activeLevel].Draw(spriteBatch, background);
+    }
+
+    /// <summary>
+    /// draws the active level in the active world
+    /// </summary>
+    /// <param name="spriteBatch">the SpriteBatch object</param>
+    public void DrawLightMap(SpriteBatch spriteBatch)
+    {
+      Levels[activeLevel].DrawLightMap(spriteBatch);
+    }
+
+    /// <summary>
+    /// sets the active level
+    /// </summary>
+    /// <param name="id">the id of the level</param>
+    public void SetLevel(int id)
+    {
+      if (id < 0)
+      {
+        _activeLevel = Levels.Count - 1;
+      }
+      else
+        _activeLevel = id % (Levels.Count);
+    }
+
+    /// <summary>
+    /// goes to the next level
+    /// </summary>
+    public void Next()
+    {
+      SetLevel(activeLevel + 1);
+    }
+
+    /// <summary>
+    /// goes to the previous level
+    /// </summary>
+    public void Prev()
+    {
+      SetLevel(activeLevel - 1);
+    }
+
+    /// <summary>
+    /// gets the spawners of the active level
+    /// </summary>
+    /// <returns>the spawner tiles</returns>
+    public List<Tile> GetSpawners() => Levels[activeLevel].GetSpawners();
+    /// <summary>
+    /// gets collisions for the current level
+    /// </summary>
+    /// <param name="r">a rectangle to check</param>
+    /// <returns>the colliding tiles</returns>
+    public List<Tile> GetCollisions(Rectangle r) => Levels[activeLevel].GetCollisions(r);
+    /// <summary>
+    /// gets the active level
+    /// </summary>
+    /// <returns>the active level</returns>
+    public Level ActiveLevel() => Levels[activeLevel];
+    /// <summary>
+    /// gets the active levels name
+    /// </summary>
+    /// <returns>the name</returns>
+    public String ActiveLevelName() => Levels[activeLevel].Name;
+    /// <summary>
+    /// gets the id of the active level
+    /// </summary>
+    /// <returns>the id</returns>
+    public int ActiveLevelNum() => activeLevel;
+  }
 }
