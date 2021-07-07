f57b7e68b0f61c28f8e0473403c9581e94ca147f
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 14:59:19 -0400

    cleanup interfaces

--- UpgradePlatformer/UpgradePlatformer/Interfaces/IDamageable.cs
+++ UpgradePlatformer/UpgradePlatformer/Interfaces/IDamageable.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace UpgradePlatformer.Interfaces
+﻿namespace UpgradePlatformer.Interfaces
 {
-    //HEADER============================================
-    //Name: Sami Chamberlain, Preston Precourt
-    //Date: 5/22/2021
-    //Purpose: Provides methods to entities that can
-    //be damaged
-    //==================================================
+  //HEADER============================================
+  //Name: Sami Chamberlain, Preston Precourt
+  //Date: 5/22/2021
+  //Purpose: Provides methods to entities that can
+  //be damaged
+  //==================================================
 
-    interface IDamageable : IGameEntity
-    {
-        //Methods
+  interface IDamageable : IGameEntity
+  {
+    //Methods
 
-        /// <summary>
-        /// Reduces health from an object
-        /// </summary>
-        /// <param name="damageAmt">Amount of damage taken</param>
-        public void TakeDamage(int damageAmt);
-    }
+    /// <summary>
+    /// Reduces health from an object
+    /// </summary>
+    /// <param name="damageAmt">Amount of damage taken</param>
+    public void TakeDamage(int damageAmt);
+  }
 }
