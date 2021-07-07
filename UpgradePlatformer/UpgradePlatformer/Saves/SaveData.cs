7c130c384dc73df715280bacdea0928093e20117
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 15:05:01 -0400

    cleanup saves

--- UpgradePlatformer/UpgradePlatformer/Saves/SaveData.cs
+++ UpgradePlatformer/UpgradePlatformer/Saves/SaveData.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UpgradePlatformer.Levels;
 using UpgradePlatformer.Upgrade_Stuff;
 
 namespace UpgradePlatformer.Saves
 {
-    /// <summary>
-    /// Container for your save game data.
-    /// Put the variables you need here, as long as it's serializable.
-    /// </summary>
-    [Serializable]
-    public class SaveData
-    {
-        public bool muted;
-        public bool valid;
-        public bool fullscreen;
-        public uint lastWorld;
-        public int money;
-        public List<LevelCollectedEntity> collectedEntities;
-        public List<Upgrade> upgrades;
-    }
+  /// <summary>
+  /// Container for your save game data.
+  /// Put the variables you need here, as long as it's serializable.
+  /// </summary>
+  [Serializable]
+  public class SaveData
+  {
+    public bool muted;
+    public bool valid;
+    public bool fullscreen;
+    public uint lastWorld;
+    public int money;
+    public List<LevelCollectedEntity> collectedEntities;
+    public List<Upgrade> upgrades;
+  }
 }
