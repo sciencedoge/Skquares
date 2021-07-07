376d4b42eb20af7ef743f027ea30a2807e631a6a
bob16795 <prestosilver.ptp@gmail.com>
2021-07-07 15:12:25 -0400

    cleanup weapons

--- UpgradePlatformer/UpgradePlatformer/Weapon/Bullet.cs
+++ UpgradePlatformer/UpgradePlatformer/Weapon/Bullet.cs
@@ -1,69 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UpgradePlatformer.Graphics;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
 using UpgradePlatformer.Entities;
-using UpgradePlatformer.Levels;
 
 namespace UpgradePlatformer.Weapon
 {
-    //HEADER==========================================
-    //Name: Sami Chamberlain, Preston Precourt
-    //Date: 5/30/2021
-    //Purpose: Creates bullets for the weapon
-    //================================================
-    class Bullet : Projectile
+  //HEADER==========================================
+  //Name: Sami Chamberlain, Preston Precourt
+  //Date: 5/30/2021
+  //Purpose: Creates bullets for the weapon
+  //================================================
+  class Bullet : Projectile
+  {
+    /// <summary>
+    /// Creates a bullet object
+    /// </summary>
+    public Bullet(Vector2 path, Vector2 location, float rotation, int cap, int hits)
+        : base(path, location, rotation, cap, hits) { }
+
+
+    /// <summary>
+    /// Checks for intersections
+    /// </summary>
+    public override void Intersects()
     {
-        /// <summary>
-        /// Creates a bullet object
-        /// </summary>
-        public Bullet(Vector2 path, Vector2 location, float rotation, int cap, int hits)
-            : base(path, location, rotation, cap, hits) { }
-
-
-        /// <summary>
-        /// Checks for intersections
-        /// </summary>
-        public override void Intersects()
+      foreach (Enemy e in EntityManager.Instance.Enemies())
+      {
+        if (hitbox.Intersects(e.Hitbox))
         {
-            foreach (Enemy e in EntityManager.Instance.Enemies())
+          if (e.IsActive)
+          {
+            e.TakeDamage(EntityManager.Instance.Player().Damage);
+
+            isActive = false;
+            if (e.CurrentHP <= 0)
             {
-                if (hitbox.Intersects(e.Hitbox))
-                {
-                    if (e.IsActive)
-                    {
-                        e.TakeDamage(EntityManager.Instance.Player().Damage);
-
-                        isActive = false;
-                        if (e.CurrentHP <= 0)
-                        {
-                            e.IsActive = false;
-                        }
-                        return;
-                    }
-                }
+              e.IsActive = false;
             }
-
-            Boss b = EntityManager.Instance.Boss();
-            if (b == null)
-                return;
-            if (hitbox.Intersects(b.Hitbox))
-            {
-                if (b.IsActive)
-                {
-                    b.TakeDamage(EntityManager.Instance.Player().Damage);
-
-                    isActive = false;
-                    if (b.CurrentHP <= 0)
-                    {
-                        b.IsActive = false;
-                    }
-                    return;
-                }
-            }
+            return;
+          }
         }
+      }
+
+      Boss b = EntityManager.Instance.Boss();
+      if (b == null)
+        return;
+      if (hitbox.Intersects(b.Hitbox))
+      {
+        if (b.IsActive)
+        {
+          b.TakeDamage(EntityManager.Instance.Player().Damage);
+
+          isActive = false;
+          if (b.CurrentHP <= 0)
+          {
+            b.IsActive = false;
+          }
+          return;
+        }
+      }
     }
+  }
 }
--- UpgradePlatformer/UpgradePlatformer/Weapon/Fireball.cs
+++ UpgradePlatformer/UpgradePlatformer/Weapon/Fireball.cs
@@ -1,91 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using UpgradePlatformer.Entities;
 using UpgradePlatformer.Graphics;
 using UpgradePlatformer.Music;
 
 namespace UpgradePlatformer.Weapon
 {
-    //HEADER===========================================
-    //Name: Sami Chamberlain, Preston Precourt
-    //Date: 6/4/2021
-    //Purpose: Creates the fireball boss projectile
-    //=================================================
-    class Fireball : Projectile
+  //HEADER===========================================
+  //Name: Sami Chamberlain, Preston Precourt
+  //Date: 6/4/2021
+  //Purpose: Creates the fireball boss projectile
+  //=================================================
+  class Fireball : Projectile
+  {
+    // particle stuffs
+    private ParticleSystem ps;
+    private ParticleProps props;
+
+    /// <summary>
+    /// Creates a fireball object
+    /// </summary>
+    public Fireball(Vector2 path, Vector2 location, float rotation)
+        : base(path, location, rotation, 10000, 1)
     {
-        // particle stuffs
-        private ParticleSystem ps;
-        private ParticleProps props;
-
-        /// <summary>
-        /// Creates a fireball object
-        /// </summary>
-        public Fireball(Vector2 path, Vector2 location, float rotation)
-            :base(path, location, rotation, 10000, 1)
-        {
-            spriteBounds = new Rectangle(28, 0, 7, 7);
-            UpdateSprite();
-            ps = new ParticleSystem();
-            props = new ParticleProps()
-            {
-                Position = new Vector2(hitbox.Center.X + 2, hitbox.Location.Y),
-                Velocity = 0.1f * path,
-                VelocityVariation = new Vector2(1.5f, 1.5f),
-                StartColor = Color.Orange,
-                EndColor = Color.Black,
-                SizeStart = 7.0f,
-                SizeEnd = 1.0f,
-                LifeTime = 0.5f
-            };
-        }
-
-        double t = 0;
-        public override void Update(GameTime gt)
-        {
-            base.Update(gt);
-            ps.Update(gt);
-
-            t += gt.ElapsedGameTime.TotalMilliseconds;
-            while (t > 100)
-            {
-                props.Position = new Vector2(hitbox.Center.X, hitbox.Center.Y);
-                ps.Emit(props);
-                t -= 100;
-            }
-        }
-
-        public override void Draw(SpriteBatch sb)
-        {
-            base.Draw(sb);
-            ps.Draw(sb);
-        }
-
-        /// <summary>
-        /// Checks for intersections
-        /// </summary>
-        public override void Intersects()
-        {
-            Player p = EntityManager.Instance.Player();
-
-            if (hitbox.Intersects(p.Hitbox))
-            {
-                if (p.IsActive)
-                {
-                    p.TakeDamage(2);
-                    SoundManager.Instance.PlaySFX("damage");
-
-                    isActive = false;
-                    if (p.CurrentHP <= 0)
-                    {
-                        p.IsActive = false;
-                    }
-                    return;
-                }
-            }
-        }
+      spriteBounds = new Rectangle(28, 0, 7, 7);
+      UpdateSprite();
+      ps = new ParticleSystem();
+      props = new ParticleProps()
+      {
+        Position = new Vector2(hitbox.Center.X + 2, hitbox.Location.Y),
+        Velocity = 0.1f * path,
+        VelocityVariation = new Vector2(1.5f, 1.5f),
+        StartColor = Color.Orange,
+        EndColor = Color.Black,
+        SizeStart = 7.0f,
+        SizeEnd = 1.0f,
+        LifeTime = 0.5f
+      };
     }
+
+    double t = 0;
+    public override void Update(GameTime gt)
+    {
+      base.Update(gt);
+      ps.Update(gt);
+
+      t += gt.ElapsedGameTime.TotalMilliseconds;
+      while (t > 100)
+      {
+        props.Position = new Vector2(hitbox.Center.X, hitbox.Center.Y);
+        ps.Emit(props);
+        t -= 100;
+      }
+    }
+
+    public override void Draw(SpriteBatch sb)
+    {
+      base.Draw(sb);
+      ps.Draw(sb);
+    }
+
+    /// <summary>
+    /// Checks for intersections
+    /// </summary>
+    public override void Intersects()
+    {
+      Player p = EntityManager.Instance.Player();
+
+      if (hitbox.Intersects(p.Hitbox))
+      {
+        if (p.IsActive)
+        {
+          p.TakeDamage(2);
+          SoundManager.Instance.PlaySFX("damage");
+
+          isActive = false;
+          if (p.CurrentHP <= 0)
+          {
+            p.IsActive = false;
+          }
+          return;
+        }
+      }
+    }
+  }
 }
